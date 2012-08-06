79ffe0278e39fb878c4f27dea958b9796e3c85ce
earlnuclear <earlnuclear@gmail.com>
2012-08-06 18:19:32 +0100

    Base for preview screen and other fixes
    Stuff

---
+++ booruReader/Preview Screen/PreviewScreenView.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace booruReader.Preview_Screen
+{
+    /// <summary>
+    /// Interaction logic for PrviewScreenView.xaml
+    /// </summary>
+    public partial class PrviewScreenView : Window
+    {
+        PreviewScreenVM PreviewVM;
+
+        public PrviewScreenView()
+        {
+            InitializeComponent();
+
+            PreviewVM = new PreviewScreenVM();
+            DataContext = PreviewVM;
+        }
+
+        private DateTime m_headerLastClicked;
+
+        private void HandleHeaderPreviewMouseDown(Object sender, MouseButtonEventArgs e)
+        {
+            m_headerLastClicked = DateTime.Now;
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                //Unmaximise window
+                if (this.WindowState != System.Windows.WindowState.Normal)
+                {
+                    this.WindowState = System.Windows.WindowState.Normal;
+
+                    //Move window to the cursor
+                    this.Left = Mouse.GetPosition(this).X - 15;
+                    this.Top = Mouse.GetPosition(this).Y - 15;
+                }
+
+                DragMove();
+            }
+        }
+
+        private void ExitButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            //PreviewVM.Closing();
+            this.Close();
+        }
+
+        private void MinimiseButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = System.Windows.WindowState.Minimized;
+        }
+
+        private void MaximiseButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState != System.Windows.WindowState.Maximized)
+            {
+                this.WindowState = System.Windows.WindowState.Maximized;
+               // LoadMoreImages();
+            }
+            else
+                this.WindowState = System.Windows.WindowState.Normal;
+        }
+
+    }
+}
